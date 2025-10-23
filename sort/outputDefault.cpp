2ea02fc6ef99cc8ff436658410ccaeaef8e49c94
mmdsdgh <s.ghiasvand2020@gmail.com>
2025-10-23 18:25:32 +0330

    change namespace in "outputDefault.cpp"

--- sort/outputDefault.cpp
+++ sort/outputDefault.cpp
@@ -1,40 +1,49 @@
 #include "mainInclude.hpp"
 
+namespace SI4
+{
+    using namespace std;
+
+    int mainSI4()
+    {
+        while (true)
+        {
+            vector<int> arr = mainArr;
+            cout << "  ---------------  Sorting Algorithms ---------------  ";
+
+            unsigned int i = 0;
+            for (; i < menu.size(); i++)
+            {
+                cout << "\n   "
+                     << i + 1 << "). " << menu[i];
+            }
+            cout << "\n   "
+                 << i + 1 << "). " << "Exit.";
+
+            int chose;
+            cout << "\n  --- Choose your function sort: ";
+            cin >> chose;
+            if (chose == 7)
+                eXit();
+
+            cout << "\n  -------------" << menu[chose - 1] << "-------------  " << endl
+                 << "   Before sorting: \n     ";
+            print(arr, arr.size());
+
+            auto sortFunc = menuFunctions[chose - 1];
+            sortFunc(arr);
+
+            cout << "   After sorting: \n     ";
+            print(arr, arr.size());
+            cout
+                << endl
+                << endl;
+        }
+        return 0;
+    }
+}
+
 int main()
 {
-    while (true)
-    {
-        std::vector<int> arr = mainArr;
-        std::cout << "  ---------------  Sorting Algorithms ---------------  ";
-
-        unsigned int i = 0;
-        for (; i < menu.size(); i++)
-        {
-            std::cout << "\n   "
-                      << i + 1 << "). " << menu[i];
-        }
-        std::cout << "\n   "
-                  << i + 1 << "). " << "Exit.";
-
-        int chose;
-        std::cout << "\n  --- Choose your function sort: ";
-        std::cin >> chose;
-        if (chose == 7)
-            eXit();
-
-        std::cout << "\n  -------------" << menu[chose - 1] << "-------------  " << std::endl
-                  << "   Before sorting: \n     ";
-        print(arr, arr.size());
-
-        auto sortFunc = menuFunctions[chose - 1];
-        sortFunc(arr);
-
-        std::cout << "   After sorting: \n     ";
-        print(arr, arr.size());
-        std::cout
-            << std::endl
-            << std::endl;
-    }
-
-    return 0;
+    return SI4::mainSI4();
 }
